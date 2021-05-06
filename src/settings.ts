1e9195db5cb8ee5d7b589896edc563492db1fdb1
AidenLx <aiden.lx@outlook.com>
2021-05-06 22:25:47 +0800

    feat(settings.ts): add setting tab for noteImportOption

--- src/settings.ts
+++ src/settings.ts
@@ -23,5 +23,70 @@ export class MNCompSettingTab extends PluginSettingTab {
     this.plugin = plugin;
   }
 
-  display(): void {}
+  display(): void {
+
+    this.containerEl.empty();
+    
+    const noteImportOption = this.plugin.settings.noteImportOption;
+
+    new Setting(this.containerEl)
+    .setName("NoteImportMode")
+    .addDropdown((dropdown) => {
+      const options: Record<NoteImportMode, string> = {
+        0: "Insert",
+        1: "Merge",
+      };
+
+      dropdown
+        .addOptions(options)
+        .setValue(noteImportOption.importMode.toString())
+        .onChange(async (value) => {
+          noteImportOption.importMode = +value;
+          await this.plugin.saveSettings();
+          this.display();
+        });
+    });
+
+    new Setting(this.containerEl)
+    .setName("NoteImportStyle")
+    .addDropdown((dropdown) => {
+      const options: Record<NoteImportStyle, string> = {
+        0: "Metadata",
+        1: "Basic",
+        2: "Full"
+      };
+
+      dropdown
+        .addOptions(options)
+        .setValue(noteImportOption.importStyle.toString())
+        .onChange(async (value) => {
+          noteImportOption.importStyle = +value;
+          await this.plugin.saveSettings();
+          this.display();
+        });
+    });
+
+    new Setting(this.containerEl)
+      .setName("blanksAroundSingleLine")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(noteImportOption.blanksAroundSingleLine)
+          .onChange(async (value) => {
+            noteImportOption.blanksAroundSingleLine = value;
+            await this.plugin.saveSettings();
+            this.display();
+          })
+      );
+    new Setting(this.containerEl)
+      .setName("updateH1")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(noteImportOption.updateH1)
+          .onChange(async (value) => {
+            noteImportOption.updateH1 = value;
+            await this.plugin.saveSettings();
+            this.display();
+          })
+      );
+  }
 }
