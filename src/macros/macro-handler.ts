2f0a4c4a4d6f9ab814354bd6ae19d99bc6255b7c
aidenlx <aiden.lx@outlook.com>
2021-10-29 22:17:42 +0800

    feat(sel-to-alias): update sel-to-alias to support import from mn data
    now the command is renamed to "AutoDef"

--- src/macros/macro-handler.ts
+++ src/macros/macro-handler.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
-import { ObsidianProtocolData } from "obsidian";
+import { MarkdownView, ObsidianProtocolData } from "obsidian";
 
 import MNComp from "../mn-main";
 import SelToAilas from "./sel-to-ailas";
@@ -10,9 +10,11 @@ export function MacroHandler(this: MNComp, params: ObsidianProtocolData): void {
     throw new TypeError("Params without macro field is passed to MacroHandler");
 
   switch (macroName) {
-    case "autodef":
-      // SelToAilas(this.app);
+    case "autodef": {
+      let mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
+      if (mdView) SelToAilas(this)(false, mdView.editor);
       break;
+    }
     default:
       console.error("unsupported macro");
       break;
@@ -23,7 +25,7 @@ export function registerMacroCmd(this: MNComp) {
   // register macros
   this.addCommand({
     id: "autodef",
-    name: "Selection to Ailases",
-    editorCallback: SelToAilas,
+    name: "AutoDef",
+    editorCheckCallback: SelToAilas(this),
   });
 }
