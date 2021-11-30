80c7780b29ccd2536aca6830c1ebbd768babf97c
aidenlx <aiden.lx@outlook.com>
2021-11-30 23:37:55 +0800

    fix(linked2folder): fix not including embeds; fix not working on preview mode

--- src/toc-tools/linked2folder.ts
+++ src/toc-tools/linked2folder.ts
@@ -1,4 +1,5 @@
 import { getApi } from "@aidenlx/folder-note-core";
+import { App, CachedMetadata, MarkdownView, TFile } from "obsidian";
 import { join } from "path";
 
 import MNComp from "../mn-main";
@@ -7,25 +8,19 @@ const CopyLinkedToFolder = (plugin: MNComp) => {
   plugin.addCommand({
     id: "linked2folder",
     name: "linked to folder",
-    editorCheckCallback: (checking, editor, view) => {
-      const srcNote = view.file,
-        links = plugin.app.metadataCache.getFileCache(srcNote)?.links,
-        folder = getApi(plugin)?.getFolderFromNote(srcNote);
-      if (!links || !folder) return false;
+    checkCallback: (checking) => {
+      const { metadataCache, workspace, fileManager } = plugin.app;
+      const srcNote = workspace.getActiveViewOfType(MarkdownView)?.file;
+      if (!srcNote) return false;
+      const cache = metadataCache.getFileCache(srcNote),
+        folder = getApi(plugin)?.getFolderFromNote(srcNote) ?? srcNote.parent;
+      if (!cache) return false;
       else if (checking) {
         return true;
       } else {
-        for (const item of links) {
-          const linkFile = plugin.app.metadataCache.getFirstLinkpathDest(
-            item.link,
-            view.file.path,
-          );
-          if (linkFile && linkFile.parent.path !== folder.path) {
-            plugin.app.fileManager.renameFile(
-              linkFile,
-              join(folder.path, linkFile.name),
-            );
-          }
+        for (const linkFile of allOutgoingLinks(srcNote, cache, plugin.app)) {
+          if (linkFile.parent.path === folder.path) continue;
+          fileManager.renameFile(linkFile, join(folder.path, linkFile.name));
         }
       }
     },
@@ -33,3 +28,20 @@ const CopyLinkedToFolder = (plugin: MNComp) => {
 };
 
 export default CopyLinkedToFolder;
+
+function* allOutgoingLinks(file: TFile, meta: CachedMetadata, app: App) {
+  const { metadataCache } = app;
+  for (const prop of ["links", "embeds"] as const) {
+    const links = meta[prop];
+    if (!links) continue;
+    for (const cache of links) {
+      const linkedFile = metadataCache.getFirstLinkpathDest(
+        cache.link,
+        file.path,
+      );
+      if (linkedFile && linkedFile.extension === "md") {
+        yield linkedFile;
+      }
+    }
+  }
+}
