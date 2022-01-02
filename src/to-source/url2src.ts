b2e775e0d7327e9e704e409e15cc30238282fa81
aidenlx <aiden.lx@outlook.com>
2022-01-02 14:52:19 +0800

    feat(url2src): support import alias from wiki; add support for radiopaedia.org

--- src/to-source/url2src.ts
+++ src/to-source/url2src.ts
@@ -3,12 +3,19 @@ import { Editor } from "obsidian";
 import { addToFrontmatter } from "../handlers/frontmatter";
 
 const url2src = async (editor: Editor) => {
-  const addUrlToSrc = (name: string, url: URL) =>
-    addToFrontmatter("sources", { [name]: url.toString() }, editor);
+  const addUrlToSrc = (id: string, url: URL) => {
+    addToFrontmatter("sources", { [id]: url.toString() }, editor);
+    let name;
+    if ((name = url.pathname.split("/").last()?.replace(/[_-]/g, " "))) {
+      addToFrontmatter("aliases", [name], editor);
+    }
+  };
   try {
     let url = new URL(await navigator.clipboard.readText());
     if (url.hostname.endsWith("wikipedia.org")) {
       addUrlToSrc("wiki", url);
+    } else if (url.hostname.endsWith("radiopaedia.org")) {
+      addUrlToSrc("radio", url);
     } else {
       throw new Error("No match for hostname: " + url.hostname);
     }
